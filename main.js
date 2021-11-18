349b09336b83188e27902a55f812a59a93bda30f
Esther Ulysse <estherulysse@Esthers-MBP.fios-router.home>
2021-11-18 10:39:03 -0500

    updates

--- main.js
+++ main.js
@@ -1,41 +1,45 @@
+const sounds = document.querySelectorAll("#sounds p input[type='checkbox']");
+
+const [metronome, kickdrum, snaredrum, hithat] = sounds;
+
+
 // Setup 'tick' sound
 const tick = new Audio('sounds/tick.mp3');
 const tock = new Audio('sounds/tock.mp3');
 const kick = new Audio('sounds/kick-drum.mp3');
 const snare = new Audio('sounds/snare-drum.mp3');
 const hihat = new Audio('sounds/hi-hat.mp3');
-let count=0;
+
+let drumCount = 0;
+let tockCount = 3;
 // This function is called every 600ms
 function update() {
 
-    count++;
-    
-    const metronomeCount = (count % 4) + 1;
-    document.querySelector("#count").innerText=metronomeCount;
-
-    if (document.querySelector("#metronome").checked) {
-    
-        if (document.querySelector("#kick-drum").checked && Number(document.querySelector("#kick-drum-timing").value)===metronomeCount)
-            kick.play()
-        if (document.querySelector("#snare-drum").checked && Number(document.querySelector("#snare-drum-timing").value)===metronomeCount)
-            snare.play()
-        if (document.querySelector("#hi-hat").checked && Number(document.querySelector("#hi-hat-timing").value)===metronomeCount)
-            hihat.play()
-
-        if (metronomeCount===4) {
-            // Play the 'tock' sound
+    if(metronome.checked) {
+        if(drumCount === tockCount) {
             tock.play();
-
+    
+            tockCount += 4
+            console.log("tock: ",drumCount);
         } else {
             // Play the 'tick' sound
             tick.play();
+    
+            console.log("tick: ", drumCount);
         }
-
-
+    drumCount +=1;
+    } else if(kick.checked){
+        kick.play();
+        drumCount += 1;
+    }else if(snare.checked){
+        snare.play();
+        drumCount += 1;
+    }else if(hihat.checked){
+        hihat.play();
+        drumCount += 1;
     }
-
+    
 }
-
 // This function sets up update() to be called every 600ms
 function setupUpdate() {
     setInterval(update, 600);
@@ -43,3 +47,9 @@ function setupUpdate() {
 
 // Call setupUpdate() once after 300ms
 setTimeout(setupUpdate, 300);
+
+
+
+
+
+
