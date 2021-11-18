bc76f751858fca9598f24aa14edb333f457d02cc
Esther Ulysse <estherulysse@Esthers-MBP.fios-router.home>
2021-11-18 01:08:52 -0500

    updates

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
+const sounds = document.querySelectorAll("#sounds p input[type='checkbox']");
+
+const [metronome, kickdrum, snaredrum, hithat] = sounds;
+
+
 // Setup 'tick' sound
 const tick = new Audio('sounds/tick.mp3');
+const tock = new Audio('sounds/tock.mp3');
+const kick = new Audio('sounds/kick-drum.mp3');
+const snare = new Audio('sounds/snare-drum.mp3');
+const hihat = new Audio('sounds/hi-hat.mp3');
 
+let drumCount = 0;
+let tockCount = 3;
 // This function is called every 600ms
 function update() {
 
-    // Play the 'tick' sound
-    tick.play();
-}
+    if(metronome.checked) {
+        if(drumCount === tockCount) {
+            tock.play();
+    
+            tockCount += 4
+            console.log("tock: ",drumCount);
+        } else {
+            // Play the 'tick' sound
+            tick.play();
+    
+            console.log("tick: ", drumCount);
+        }
+    drumCount +=1;
+    }
 
+    
+}
 // This function sets up update() to be called every 600ms
 function setupUpdate() {
     setInterval(update, 600);
@@ -15,3 +39,9 @@ function setupUpdate() {
 
 // Call setupUpdate() once after 300ms
 setTimeout(setupUpdate, 300);
+
+
+
+
+
+
